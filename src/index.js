23075a248443ce0746c5cbafd1a451847f74e5d1
ChrissDir <chriss.dirand@laposte.net>
2023-09-04 17:00:53 +0200

    lundi après-midi

--- src/index.js
+++ src/index.js
@@ -1,153 +1,82 @@
 import "./style.scss";
 
 const toggleModeButton = document.querySelector('#darkmodbtn');
-const elements = [
-  document.body,
-  document.querySelector('#container'),
-  document.querySelector('#searchbar'),
-  document.querySelector('#loupe'),
-  document.querySelector('#selection'),
-  document.querySelector('#todoTask'),
-  document.querySelector('#DoingTask'),
-  document.querySelector('#DoneTask')
-];
-const contenu = document.querySelectorAll('.contenu');
-const buttons = document.querySelectorAll('button');
-
-toggleModeButton.addEventListener('click', () => {
-  elements.forEach(element => {
-    element.classList.toggle('dark-theme');
-  });
-  buttons.forEach(button => {
-    button.classList.toggle('dark-theme');
-  });
-  contenu.forEach(cont => {
-    cont.classList.toggle('dark-theme');
-  })
-});
-
+toggleModeButton.addEventListener('click', toggleDarkMode);
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-if (prefersDarkScheme.matches) {
-  elements.forEach(element => {
-    element.classList.add('dark-theme');
-  });
-  buttons.forEach(button => {
-    button.classList.add('dark-theme');
-  });
-  contenu.forEach(cont => {
-    cont.classList.add('dark-theme');
-  })
-} else {
-  elements.forEach(element => {
-    element.classList.remove('dark-theme');
-  });
-  buttons.forEach(button => {
-    button.classList.remove('dark-theme');
-  });
-  contenu.forEach(cont => {
-    cont.classList.remove('dark-theme');
-  })
+function toggleDarkMode() {
+  let elements = document.getElementsByTagName("*");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle("dark-theme");
+  }
 }
 
-/* // Sélection des éléments du DOM
-const taskInput = document.querySelector('#task-input');
-const taskList = document.querySelector('#task-list');
-const filterButtons = document.querySelectorAll('.filter-button');
-const clearCompletedButton = document.querySelector('#supprimer');
-
-// Écouteur d'événement pour l'ajout d'une nouvelle tâche
-taskInput.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const taskText = taskInput.value.trim();
-  if (taskText !== '') {
-    addTask(taskText);
-    taskInput.value = '';
-  }
-});
-
-// Fonction pour ajouter une nouvelle tâche
-function addTask(taskText) {
-  const taskItem = document.createElement('li');
-  taskItem.innerHTML = `
-    <input type="checkbox" class="task-checkbox">
-    <span class="task-text">${taskText}</span>
-    <button class="delete-button">Supprimer</button>
-  `;
-  taskList.appendChild(taskItem);
-}
-
-// Écouteur d'événement pour marquer une tâche comme terminée
-taskList.addEventListener('change', (e) => {
-  if (e.target.classList.contains('task-checkbox')) {
-    const taskItem = e.target.parentNode;
-    taskItem.classList.toggle('completed');
-  }
-});
-
-// Écouteur d'événement pour supprimer une tâche
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete-button')) {
-    const taskItem = e.target.parentNode;
-    taskList.removeChild(taskItem);
-  }
-});
-
-// Écouteurs d'événement pour les boutons de filtrage
-filterButtons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const filter = e.target.dataset.filter;
-    filterTasks(filter);
-  });
-});
-
-// Fonction pour filtrer les tâches
-function filterTasks(filter) {
-  const taskItems = taskList.children;
-  for (const taskItem of taskItems) {
-    switch (filter) {
-      case 'all':
-        taskItem.style.display = 'flex';
-        break;
-      case 'active':
-        if (taskItem.classList.contains('completed')) {
-          taskItem.style.display = 'none';
-        } else {
-          taskItem.style.display = 'flex';
-        }
-        break;
-      case 'completed':
-        if (taskItem.classList.contains('completed')) {
-          taskItem.style.display = 'flex';
-        } else {
-          taskItem.style.display = 'none';
-        }
-        break;
+function addDarkModeByDefault() {
+  if (prefersDarkScheme.matches) {
+    let elements = document.getElementsByTagName("*");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("dark-theme");
     }
   }
 }
 
-// Écouteur d'événement pour effacer toutes les tâches terminées
-clearCompletedButton.addEventListener('click', () => {
-  const completedTasks = taskList.querySelectorAll('.completed');
-  completedTasks.forEach((task) => {
-    taskList.removeChild(task);
+addDarkModeByDefault();
+
+const btnfermer = document.querySelector('#btnfermer');
+const modal = document.querySelector('#modal');
+btnfermer.addEventListener('click', function(e){
+  e.preventDefault();
+  modal.style.display = 'none';
+});
+
+const deleteAllButton = document.querySelector("#deleteAll");
+deleteAllButton.addEventListener("click", function() {
+  const doneTaskSection = document.querySelector("#DoneTask");
+  const doneTaskDivs = doneTaskSection.querySelectorAll(".task");
+  doneTaskDivs.forEach(function(taskDiv) {
+    taskDiv.remove();
   });
 });
 
-// Récupération des tâches depuis le stockage local
-window.addEventListener('DOMContentLoaded', () => {
-  const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  storedTasks.forEach((taskText) => {
-    addTask(taskText);
-  });
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('supprimer')) {
+    const taskElement = event.target.closest('.task');
+    taskElement.remove();
+  }
 });
 
-// Mise à jour du stockage local lors de la modification des tâches
-taskList.addEventListener('change', () => {
-  const tasks = Array.from(taskList.children).map((taskItem) => {
-    const taskText = taskItem.querySelector('.task-text').textContent;
-    return taskText;
-  });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-});  */
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('addTask')) {
+    const parentElementId = event.target.parentNode.id;
+    modal.style.display = 'flex';
+    btnmodal.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const nameTask = document.getElementById('nameTask').value;
+      const description = document.getElementById('descrimodal').value;
+      const date = document.getElementById('datemodal').value;
+
+      const template = document.querySelector('.newTaskTemplate');
+      const newTask = template.content.cloneNode(true);
+
+      const taskNameElement = newTask.querySelector('.nomDeTâche');
+      taskNameElement.textContent = nameTask;
+
+      const taskDateElement = newTask.querySelector('.date');
+      taskDateElement.textContent = date;
+
+      const taskDescriptionElement = newTask.querySelector('.description');
+      taskDescriptionElement.textContent = description;
+
+      const parentElement = document.getElementById(parentElementId);
+      parentElement.appendChild(newTask);
+
+      // Réinitialiser les valeurs du formulaire
+      document.getElementById('nameTask').value = '';
+      document.getElementById('descrimodal').value = '';
+      document.getElementById('datemodal').value = '';
+
+      modal.style.display = 'none';
+    });
+  }
+});
