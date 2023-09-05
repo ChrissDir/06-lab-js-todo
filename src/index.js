9cfd28f3bd41a81189d5acaa88842c050da3993a
Anarchyfear <anarchyfear8@gmail.com>
2023-09-05 19:20:59 +0200

    Fix des tâches en double

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./style.scss";
 
 const toggleModeButton = document.querySelector('#darkmodbtn');
-toggleModeButton.addEventListener('click', toggleDarkMode);
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const btnfermer = document.querySelector('#btnfermer');
+const modal = document.querySelector('#modal');
+const deleteAllButton = document.querySelector("#deleteAll");
+let parentElementId;
+
+toggleModeButton.addEventListener('click', toggleDarkMode);
 
 function toggleDarkMode() {
   let elements = document.getElementsByTagName("*");
@@ -22,14 +27,11 @@ function addDarkModeByDefault() {
 
 addDarkModeByDefault();
 
-const btnfermer = document.querySelector('#btnfermer');
-const modal = document.querySelector('#modal');
 btnfermer.addEventListener('click', function(e){
   e.preventDefault();
   modal.style.display = 'none';
 });
 
-const deleteAllButton = document.querySelector("#deleteAll");
 deleteAllButton.addEventListener("click", function() {
   const doneTaskSection = document.querySelector("#DoneTask");
   const doneTaskDivs = doneTaskSection.querySelectorAll(".task");
@@ -39,7 +41,7 @@ deleteAllButton.addEventListener("click", function() {
 });
 
 document.addEventListener('click', function(event) {
-  if (event.target.classList.contains('supprimer')) {
+  if (event.target.classList.contains('can')) {
     const taskElement = event.target.closest('.task');
     taskElement.remove();
   }
@@ -47,36 +49,43 @@ document.addEventListener('click', function(event) {
 
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('addTask')) {
-    const parentElementId = event.target.parentNode.id;
+    parentElementId = event.target.parentNode.id;
     modal.style.display = 'flex';
-    btnmodal.addEventListener("click", function(e) {
-      e.preventDefault();
-
-      const nameTask = document.getElementById('nameTask').value;
-      const description = document.getElementById('descrimodal').value;
-      const date = document.getElementById('datemodal').value;
-
-      const template = document.querySelector('.newTaskTemplate');
-      const newTask = template.content.cloneNode(true);
-
-      const taskNameElement = newTask.querySelector('.nomDeTâche');
-      taskNameElement.textContent = nameTask;
-
-      const taskDateElement = newTask.querySelector('.date');
-      taskDateElement.textContent = date;
-
-      const taskDescriptionElement = newTask.querySelector('.description');
-      taskDescriptionElement.textContent = description;
-
-      const parentElement = document.getElementById(parentElementId);
-      parentElement.appendChild(newTask);
-
-      // Réinitialiser les valeurs du formulaire
-      document.getElementById('nameTask').value = '';
-      document.getElementById('descrimodal').value = '';
-      document.getElementById('datemodal').value = '';
-
-      modal.style.display = 'none';
-    });
   }
+});
+
+btnmodal.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  const nameTask = document.getElementById('nameTask').value;
+  const description = document.getElementById('descrimodal').value;
+  const date = document.getElementById('datemodal').value;
+
+  const template = document.querySelector('.newTaskTemplate');
+  const newTask = template.content.cloneNode(true);
+
+  const taskNameElement = newTask.querySelector('.nomDeTâche');
+  taskNameElement.textContent = nameTask;
+
+  const taskDateElement = newTask.querySelector('.date');
+  taskDateElement.textContent = date;
+
+  const taskDescriptionElement = newTask.querySelector('.description');
+  taskDescriptionElement.textContent = description;
+
+  const parentElement = document.getElementById(parentElementId);
+  parentElement.appendChild(newTask);
+
+  document.getElementById('nameTask').value = '';
+  document.getElementById('descrimodal').value = '';
+  document.getElementById('datemodal').value = '';
+
+  modal.style.display = 'none';
+
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('can')) {
+      const taskElement = event.target.closest('.task');
+      taskElement.remove();
+    }
+  });
 });
